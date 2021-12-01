f0b0c6f27e28804887700572e913174013fe4b6a
Himanshu Ranjan <himanshuimpdoc@gmail.com>
2021-12-01 13:23:41 +0530

    First commit

---
+++ Lecture 3/output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a = 24;
+
+    if(a>0){
+        cout<< "Love";
+    }else if(a == 24){
+        cout<< "Myself";
+    }else{
+        cout<< "Samjha";
+    }
+
+    cout<<a;
+    
+}
